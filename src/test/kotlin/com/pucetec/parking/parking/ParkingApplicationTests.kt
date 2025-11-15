b8b97cda86295cfc3e30229b93cfa6e44c070377
andres14romero <desarrollo4@corsinf.com>
2025-11-15 10:54:54 -0500

    Initial project structure

---
+++ src/test/kotlin/com/pucetec/parking/parking/ParkingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pucetec.parking.parking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
